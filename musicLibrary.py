d5d529df308c7919092ab7ccad5bd769251dff32
Madhav Tiwari <tiwarimadhav2309@gmail.com>
2025-06-11 09:02:26 +0530

    Initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "nanchaku": "https://www.youtube.com/watch?v=VjJtH7xM8G4",
+    "bure": "https://www.youtube.com/watch?v=YlGTkStN5dw",
+    "hanuman": "https://www.youtube.com/watch?v=BLlTFapgvOo",
+    "chunnari": "https://www.youtube.com/watch?v=6z1U-kJ3xJE",
+}
